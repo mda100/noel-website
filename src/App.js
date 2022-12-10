deeb6590a2b49d5dee37511940de777050077541
mda100 <mdadams10@gmail.com>
2022-12-09 22:06:38 -0800

    basic carousel and project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Carousel from './components/Carousel/Carousel';
+import imageData from './assets/imageData';
 
 function App() {
   return (
     <div className="App">
+      {/* header */}
+      <Carousel images={imageData}/>
+      {/* footer */}
     </div>
   );
 }
