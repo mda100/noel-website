e4949ba2c9824e2838547d0034da1ce612bc6b78
Martin Adams <mdadams10@gmail.com>
2022-12-14 19:44:25 -0800

    again

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -5,9 +5,6 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styles from './Carousel.module.css';
 
-
-
-
 const ImgBlock = ({ image }) => {
   let isSafari = !(navigator.userAgent.indexOf('Chrome') > -1) && (navigator.userAgent.indexOf("Safari") > -1);
   let block;
