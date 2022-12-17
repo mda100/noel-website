477ae089e69fa67d67e1e9abacca1ebd9f1a340e
Martin Adams <mdadams10@gmail.com>
2022-12-16 19:17:35 -0800

    deployment hack

--- src/scripts/pre_deploy.sh
+++ src/scripts/pre_deploy.sh
@@ -1,3 +1,7 @@
 DIR=$(dirname "$0")
 
 node $DIR/imageFileWriter.js
+# TODO: compression script runs, then file write runs on OLD folder
+# need to be run twice for successful deployment
+# this is a quick harmless hack for now, but ideally we can figure out why we have that behavior
+node $DIR/imageFileWriter.js
