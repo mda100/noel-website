c00c67e42fa3cd0e264950b088a5607fe44fd3ab
Martin Adams <mdadams10@gmail.com>
2022-12-16 18:17:05 -0800

    shuffle on load

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Carousel from './components/Carousel/Carousel';
 import Text from './components/Text/Text';
 import imageData from './assets/imageData.json';
 
+const images = imageData.data.sort((a, b) => 0.5 - Math.random());
+
 function App() {
   return (
     <div className="App">
       <Text text={'noel website dot website'} />
-      <Carousel images={imageData.data}/>
+      <Carousel images={images}/>
       <Text text={'brought to you by alice wu and marsrocks, llc'} />
     </div>
   );
